b6766e7678f562e75857c3243137fbf80494f13e
Noah A Klem <noahaklem@gmail.com>
2023-11-13 18:45:33 -0700

    api_key routes added

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  post 'api-keys', to: 'api_keys#create'
+  delete 'api-keys', to: 'api_keys#destroy'
+  get 'api-keys', to: 'api_keys#index'
+
+  
 end
